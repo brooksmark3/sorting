64e6fd0d54b0d8bc0583b909ebd4c0ce3a9328c1
Shreya Wadhwa <shreyawadhwa369@gmail.com>
2020-04-08 11:36:15 -0500

    bubblesort working and passing specs w/ counter

--- bubblesort.spec.js
+++ bubblesort.spec.js
@@ -1,1 +1,18 @@
 //specs go here
+describe('Bubble Sort', function(){
+    beforeEach(function(){
+        counter = 0;
+    })
+    it('handles an empty array', function(){
+      expect( bubbleSort([]) ).toEqual( [] );
+      expect(counter).toEqual(0);
+    });
+    it('handles an unsorted array', function(){
+        expect( bubbleSort([6,3,9,2,8,1]) ).toEqual( [1,2,3,6,8,9] );
+        expect(counter).toEqual(10);
+    });
+    it('handles a sorted array', function(){
+        expect( bubbleSort([1,2,3,4,5,6,8,9]) ).toEqual( [1,2,3,4,5,6,8,9] );
+        expect(counter).toEqual(0);
+    });
+  });
